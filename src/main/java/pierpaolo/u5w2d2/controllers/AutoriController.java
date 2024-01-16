a425a1f62a1e925c027a3f4bc81428c0988d23aa
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2024-01-16 16:32:30 +0100

    Finish autori endpoints

--- src/main/java/pierpaolo/u5w2d2/controllers/AutoriController.java
+++ src/main/java/pierpaolo/u5w2d2/controllers/AutoriController.java
@@ -1,10 +1,10 @@
 package pierpaolo.u5w2d2.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import pierpaolo.u5w2d2.entities.Autore;
+import pierpaolo.u5w2d2.entities.Post;
 import pierpaolo.u5w2d2.services.AutoreService;
 
 import java.util.List;
@@ -16,4 +16,15 @@ public class AutoriController {
     private AutoreService autoreService;
     @GetMapping
     public List<Autore> getAutori(){ return autoreService.getAutori();}
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Autore savepost(@RequestBody Autore body){return autoreService.save(body);}
+    @GetMapping("/{id}")
+    public Autore findById(@PathVariable int id){return autoreService.findById(id);}
+    @PutMapping("/{id}")
+    public Autore findIdUpdate(@PathVariable int id, @RequestBody Autore body){
+        return this.autoreService.findByIdAndUpdate(id, body);
+    }
+    @DeleteMapping("/{id}")
+    public void findIdDelete(@PathVariable int id){ this.autoreService.findByIdAndDelete(id);}
 }
